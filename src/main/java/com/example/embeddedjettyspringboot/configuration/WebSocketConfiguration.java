39c00f77f37b133ec90314ab6eeefdcc47c2265a
AnatolSalau <salovanatoly5@mail.ru>
2022-09-28 12:24:31 +0300

    JavaFXSpringBoot_EmbeddedTomcat

--- src/main/java/com/example/embeddedjettyspringboot/configuration/WebSocketConfiguration.java
+++ src/main/java/com/example/embeddedjettyspringboot/configuration/WebSocketConfiguration.java
@@ -2,9 +2,7 @@ package com.example.embeddedjettyspringboot.configuration;
 
 import com.example.embeddedjettyspringboot.websocket.WSPong;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -12,7 +10,6 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 @EnableWebSocket
 public class WebSocketConfiguration implements WebSocketConfigurer {
-
 
     @Autowired
     WSPong wsPong;
